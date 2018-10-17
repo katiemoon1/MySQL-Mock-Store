4448c07a9e4fc7c81ea4e9b63d0c25b191e6862f
Moon Child <kaitline.moon@gmail.com>
2018-10-17 18:57:13 -0400

    Added prompts and a function to update stock.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var cTable = require("console.table");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -13,14 +14,52 @@ connection.connect(function(error) {
     if (error) throw error;
     connection.query("SELECT * FROM products", function(error, results, fields) {
         if (error) throw error;
-        console.log(results);
+        console.table(results);
+        goShopping();
     });
-    // goShopping();
   });
 
-// function goShopping() {
-//     inquirer
-//         .prompt({
+function goShopping() {
+    inquirer
+        .prompt([
+            {
+                name: "item",
+                type: "input",
+                message: "What is the ID of the product you would like to buy?",
+                validate: function(value) {
 
-//         })
-// }
+                }
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: "How many would you like to purchase?"
+            }
+        ])
+};
+
+
+// Function for updating the inventory
+function updateInventory() {
+    console.log("Updating the inventory!");
+
+    if (answer.quantity < stock_quantity) {
+        connection.query(
+            "UPDATE products SET ? WHERE ?",
+            [
+                {
+                    stock_quantity: answer.quantity
+                },
+                {
+                    id: answer.item
+                }
+            ],
+            function(error, results) {
+                console.log(results.affectedRows + " products updated!");
+            }
+        )
+    } else {
+        console.log("There is not enough left in stock!")
+    }
+
+};
