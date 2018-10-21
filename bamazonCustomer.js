064709a8f6d7181369ef32b3c697498fbdeda475
Moon Child <kaitline.moon@gmail.com>
2018-10-21 16:53:21 -0400

    making progress, the app now runs through and completes a purchase!

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,25 +1,31 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
-var cTable = require("console.table");
+const mysql = require("mysql")
+const inquirer = require("inquirer")
+const cTable = require("console.table")
 
-var connection = mysql.createConnection({
+// Creating the connection information for the database
+const connection = mysql.createConnection({
     host: "localhost",
     port: 8889,
     user: "root",
     password: "",
     database: "bamazon"
-});
+})
 
+// Connecting to the MySQL server and database
 connection.connect(function(error) {
-    if (error) throw error;
-    connection.query("SELECT * FROM products", function(error, results, fields) {
-        if (error) throw error;
-        console.table(results);
-        goShopping();
-    });
-  });
+    if (error) throw error
+    goShopping()
+})
 
+// The function that allows the user to go shopping and purchase particular quantities of items
 function goShopping() {
+
+    // Generating the table that displays all of the products, their prices, and quantities
+    connection.query("SELECT * FROM products", function(error, results, fields) {
+        if (error) throw error
+        console.table(results)
+    
+    // The question prompt that walks the user through the buying process
     inquirer
         .prompt([
             {
@@ -27,39 +33,53 @@ function goShopping() {
                 type: "input",
                 message: "What is the ID of the product you would like to buy?",
                 validate: function(value) {
-
+                    if (isNaN(value) === false) {
+                        return true;
+                    } else {
+                        return "Please input a number!";
+                    }
                 }
             },
             {
                 name: "quantity",
                 type: "input",
-                message: "How many would you like to purchase?"
+                message: "How many would you like to purchase?",
+                validate: function(value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    } else {
+                        return "Please input a number!"
+                    }
+                }
             }
         ])
-};
+        .then(function(answer) {
+            let selectedItem = answer.item 
+            let selectedId = results[selectedItem]
+            let selectedQuantity = answer.quantity
 
+            if (selectedQuantity < selectedId.stock_quantity) {
 
-// Function for updating the inventory
-function updateInventory() {
-    console.log("Updating the inventory!");
-
-    if (answer.quantity < stock_quantity) {
-        connection.query(
-            "UPDATE products SET ? WHERE ?",
-            [
-                {
-                    stock_quantity: answer.quantity
-                },
-                {
-                    id: answer.item
-                }
-            ],
-            function(error, results) {
-                console.log(results.affectedRows + " products updated!");
+                connection.query(
+                    "UPDATE products SET ? WHERE ?",
+                    [
+                        {
+                            stock_quantity: selectedId.stock_quantity - selectedQuantity
+                        },
+                        {
+                            id: selectedId.id
+                        }
+                    ],
+                    function(error, results) {
+                        if (error) throw error
+                        console.log(results.affectedRows + " products updated!")
+                    }
+                )
+            } else {
+                console.log("There is not enough left in stock!")
             }
-        )
-    } else {
-        console.log("There is not enough left in stock!")
-    }
-
-};
+        
+        })
+    
+    })
+}
